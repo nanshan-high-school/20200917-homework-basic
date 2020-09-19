4b099657e6ec6b3e75ad9adaffd2bf89ba98be5f
Yukisakura <yukisecret1@gmail.com>
2020-09-19 11:51:34 +0800

    750327作業
    新增作業~請過目

---
+++ 750327/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "請輸入你的數字: ";
+    cin >> num;
+    if (num % 3 == 0) {
+      cout << "這是三的倍數";
+    }
+    else {
+        cout << "這不是三的倍數";
+    }
+}
