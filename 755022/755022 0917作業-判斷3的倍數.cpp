8f7ac4fcecf0cf767fa6c35bb683400b423801e9
小克 <goodjack@users.noreply.github.com>
2020-09-24 00:25:22 +0800

    Update 755022 0917作業-判斷3的倍數.cpp

--- 755022/755022 0917作業-判斷3的倍數.cpp
+++ 755022/755022 0917作業-判斷3的倍數.cpp
@@ -1,14 +1,14 @@
 ﻿#include <iostream>
 using namespace std;
+
 int main() {
    int num;
-   cout<<"輸入數字";
-   cin>>num;
+   cout << "輸入數字";
+   cin >> num;
 
-   if (num %3==0) {
-     cout<<"3的倍數";
-   }
-   else {
-     cout<<"非3的倍數";
+   if (num % 3 == 0) {
+     cout << "3的倍數";
+   } else {
+     cout << "非3的倍數";
    }
 }
