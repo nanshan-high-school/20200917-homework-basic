d2d0e6929b4129a7e6f28183f178e79d5f4d80ef
小克 <goodjack@users.noreply.github.com>
2020-09-24 00:16:57 +0800

    Update 292_28_755001 _葉方叡_判斷三的倍數.cpp

--- 755001/292_28_755001 _葉方叡_判斷三的倍數.cpp
+++ 755001/292_28_755001 _葉方叡_判斷三的倍數.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main(){
     int inputnum;
     
-    while(cin >> inputnum){
-        if(inputnum  %3 == 0){
+    while (cin >> inputnum) {
+        if (inputnum % 3 == 0) {
             cout << "是三的倍數";
         } else {
             cout << "非三的倍數";
