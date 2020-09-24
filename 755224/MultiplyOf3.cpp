342915b7832e7a1b08bc79ea63052b6ca9b6724e
BrineYT <60344483+BrineYT@users.noreply.github.com>
2020-09-24 19:02:28 +0800

    Update MultiplyOf3.cpp

--- 755224/MultiplyOf3.cpp
+++ 755224/MultiplyOf3.cpp
@@ -5,7 +5,7 @@ int main() {
   cout << "Enter a number and I'll see if it's a multiply of 3\nYour number: ";
   int num;
   cin >> num;
-  if (num % 3 == 0) {
+  if (num % 3 == 0){
     cout << num << " is a multiply of three";
   } else {
     cout << num << " is not a multiply of three";
