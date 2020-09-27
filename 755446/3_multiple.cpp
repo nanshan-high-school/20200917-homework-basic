d231792eb0de02b9da3e134d19187dca9b77e046
林柏崴 <wayne20051211@gmail.com>
2020-09-27 20:52:57 +0800

    typographic_correction

--- 755446/3_multiple.cpp
+++ 755446/3_multiple.cpp
@@ -7,8 +7,7 @@ int main() {
     cout << "請輸入數字：";
     cin >> num;
 
-    if (num % 3 == 0)
-    {
+    if (num % 3 == 0) {
       cout << num << "是3的倍數";
     } else {
       cout << num << "不是3的倍數";
